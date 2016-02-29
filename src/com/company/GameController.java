9bae5123d35257b1aca03c6627496091378831cc
glupingane <torstein_alvern5557@hotmail.com>
2016-02-29 22:28:19 +0100

    suppers

--- src/com/company/GameController.java
+++ src/com/company/GameController.java
@@ -18,7 +18,7 @@ public class GameController {
         //
         String name = gui.getPlayerName("A");
         pa = new Player(name);
-        pa.setCharacter('X'); // Test
+        pa.setCharacter('X');
 
         name = gui.getPlayerName("B");
         pb = new Player(name);
